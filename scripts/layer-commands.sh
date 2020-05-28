ec9b53aa0d2673d6ddc7572e2c44ef473b8d1c3a
Jonathan Mettes <jonathan@jmettes.com>
2020-05-28 17:54:44 +1000

    Upload to S3

--- scripts/layer-commands.sh
+++ scripts/layer-commands.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-# mkdir python
 pip install -r ../src/requirements.txt -t python/lib/python3.6/site-packages/
+zip -r lambda-layer.zip python
+
+# ideally this would be done with terraform
+# but it's annoying I have to build this within a lambci/lambda:python-3.6 docker image
+# it's a two step process, lambda layer needs to exist before lambda can be deployed
+export AWS_DEFAULT_REGION=ap-southeast-2
+export AWS_REGION=ap-southeast-2
+aws s3 cp lambda-layer.zip s3://insar-atmospheric-correction-dev
+aws lambda publish-layer-version --layer-name insar-atmospheric-correction-python36 --content S3Bucket=insar-atmospheric-correction-dev,S3Key=lambda/lambda-layer.zip --compatible-runtimes python3.6
